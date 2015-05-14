153d6e3614adba1d4cefef964d904d444eb3b1dd
Yaming <yamingd@qq.com>
2015-05-14 15:07:56 +0800

    fix: client deviceToken of ios

--- Gateway/src/main/java/com/argo/qpush/gateway/handler/MessageHandlerPoolTasks.java
+++ Gateway/src/main/java/com/argo/qpush/gateway/handler/MessageHandlerPoolTasks.java
@@ -1,6 +1,8 @@
 package com.argo.qpush.gateway.handler;
 
-import com.argo.qpush.gateway.ServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,11 +14,15 @@ import java.util.Properties;
  * Created by user on 1/22/15.
  */
 @Component
-public class MessageHandlerPoolTasks implements InitializingBean {
+public class MessageHandlerPoolTasks implements InitializingBean, DisposableBean {
+
+    protected static Logger logger = LoggerFactory.getLogger(MessageHandlerPoolTasks.class);
 
     public static MessageHandlerPoolTasks instance = null;
 
     private ThreadPoolTaskExecutor poolTaskExecutor;
+
+    private volatile boolean stopping;
 
     @Autowired
     private Properties appConfig;
@@ -25,6 +31,8 @@ public class MessageHandlerPoolTasks implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         instance = this;
 
+        stopping = false;
+
         int limit = Integer.parseInt(appConfig.getProperty("handler.executors", "100"));
 
         poolTaskExecutor = new ThreadPoolTaskExecutor();
@@ -32,9 +40,33 @@ public class MessageHandlerPoolTasks implements InitializingBean {
         poolTaskExecutor.setMaxPoolSize(limit);
         poolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
         poolTaskExecutor.afterPropertiesSet();
+
+        // pool status
+        Thread thread3 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (!stopping) {
+
+                    logger.info("{}", poolTaskExecutor.getThreadPoolExecutor());
+
+                    try {
+                        Thread.sleep(10 * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread3.start();
     }
 
     public ThreadPoolTaskExecutor getExecutor() {
         return poolTaskExecutor;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        stopping = true;
     }
 }

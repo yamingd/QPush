a1f45a1cef71144cb29b5e9aafa7c646d479c709
Yaming <yamingd@qq.com>
2015-07-13 18:12:15 +0800

    fix Ack Connection issue

--- Gateway/src/main/java/com/argo/qpush/gateway/handler/MobileMessageHandler.java
+++ Gateway/src/main/java/com/argo/qpush/gateway/handler/MobileMessageHandler.java
@@ -52,11 +52,11 @@ public class MobileMessageHandler extends ChannelInboundHandlerAdapter {
         }
         MetricBuilder.requestMeter.mark();
 
-        final PBAPNSEvent cc;
+        final PBAPNSEvent pbapnsEvent;
 
         try {
             byte[] bytes = (byte[]) msg;
-            cc = PBAPNSEvent.newBuilder().mergeFrom(bytes).build();
+            pbapnsEvent = PBAPNSEvent.newBuilder().mergeFrom(bytes).build();
         } catch (Exception e) {
             logger.error("Invalid Data Package.", e);
             ctx.close();
@@ -66,49 +66,50 @@ public class MobileMessageHandler extends ChannelInboundHandlerAdapter {
         ReferenceCountUtil.release(msg);
 
         if (logger.isDebugEnabled()){
-            logger.debug("Got Message. {}", cc);
+            logger.debug("Got Message. {}", pbapnsEvent);
         }
 
-        if (StringUtils.isEmpty(cc.getUserId()) || cc.getOp() <= 0){
+        if (StringUtils.isEmpty(pbapnsEvent.getUserId()) || pbapnsEvent.getOp() <= 0){
             logger.error("Invalid Client!! so close connection!! ");
             ctx.close();
             return;
         }
 
-        if (cc.getTypeId() == PBAPNSEvent.DeviceTypes.Android_VALUE){
+        if (pbapnsEvent.getTypeId() == PBAPNSEvent.DeviceTypes.Android_VALUE){
             MetricBuilder.clientAndroidMeter.mark();
-        }else if (cc.getTypeId() == PBAPNSEvent.DeviceTypes.iOS_VALUE){
+        }else if (pbapnsEvent.getTypeId() == PBAPNSEvent.DeviceTypes.iOS_VALUE){
             MetricBuilder.clientIOSMeter.mark();
         }
 
-        if(cc.getOp() == PBAPNSEvent.Ops.Online_VALUE){
+        if(pbapnsEvent.getOp() == PBAPNSEvent.Ops.Online_VALUE){
 
-            Connection conn = ConnectionKeeper.get(cc.getAppKey(), cc.getUserId());
+            Connection conn = ConnectionKeeper.get(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId());
             if (null != conn){
-                logger.error("你已经在线了!. KickOff cc={}, conn={}", cc, conn);
-                ack(ctx, cc, MULTI_CLIENTS);
+                logger.error("你已经在线了!. KickOff pbapnsEvent={}, conn={}", pbapnsEvent, conn);
+                ack(ctx, conn, pbapnsEvent, MULTI_CLIENTS);
             }
 
             conn = new Connection(ctx.channel());
-            conn.setUserId(cc.getUserId());
-            conn.setAppKey(cc.getAppKey());
-            ConnectionKeeper.add(cc.getAppKey(), cc.getUserId(), conn);
+            conn.setUserId(pbapnsEvent.getUserId());
+            conn.setAppKey(pbapnsEvent.getAppKey());
+            ConnectionKeeper.add(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId(), conn);
             //记录客户端
-            MessageHandlerPoolTasks.instance.getExecutor().submit(new OnNewlyAddThread(cc));
-            ack(ctx, cc, SYNC);
+            MessageHandlerPoolTasks.instance.getExecutor().submit(new OnNewlyAddThread(pbapnsEvent));
+            ack(ctx, conn, pbapnsEvent, SYNC);
             if (logger.isDebugEnabled()){
-                logger.debug("Got Online Message and handle DONE. {}", cc);
+                logger.debug("Got Online Message and handle DONE. {}", pbapnsEvent);
             }
-        }else if(cc.getOp() == PBAPNSEvent.Ops.KeepAlive_VALUE){
+        }else if(pbapnsEvent.getOp() == PBAPNSEvent.Ops.KeepAlive_VALUE){
             //心跳
-            ack(ctx, cc, SYNC);
+            Connection conn = ConnectionKeeper.get(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId());
+            ack(ctx, conn, pbapnsEvent, SYNC);
 
-        }else if(cc.getOp() == PBAPNSEvent.Ops.Sleep_VALUE){
+        }else if(pbapnsEvent.getOp() == PBAPNSEvent.Ops.Sleep_VALUE){
             MessageHandlerPoolTasks.instance.getExecutor().submit(new Runnable() {
 
                 @Override
                 public void run() {
-                    Client c0 = ClientServiceImpl.instance.findByUserId(cc.getUserId());
+                    Client c0 = ClientServiceImpl.instance.findByUserId(pbapnsEvent.getUserId());
                     if (c0 != null) {
                         ClientServiceImpl.instance.updateStatus(c0.getId(), 2);
                     }
@@ -116,50 +117,52 @@ public class MobileMessageHandler extends ChannelInboundHandlerAdapter {
 
             });
             //心跳
-            ack(ctx, cc, SYNC);
+            Connection conn = ConnectionKeeper.get(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId());
+            ack(ctx, conn, pbapnsEvent, SYNC);
 
-        }else if(cc.getOp() == PBAPNSEvent.Ops.Awake_VALUE){
-            Connection conn = ConnectionKeeper.get(cc.getAppKey(), cc.getUserId());
+        }else if(pbapnsEvent.getOp() == PBAPNSEvent.Ops.Awake_VALUE){
+            Connection conn = ConnectionKeeper.get(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId());
             if (null == conn){
                 conn = new Connection(ctx.channel());
-                conn.setUserId(cc.getUserId());
-                conn.setAppKey(cc.getAppKey());
-                ConnectionKeeper.add(cc.getAppKey(), cc.getUserId(), conn);
+                conn.setUserId(pbapnsEvent.getUserId());
+                conn.setAppKey(pbapnsEvent.getAppKey());
+                ConnectionKeeper.add(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId(), conn);
             }
 
             //记录客户端
-            MessageHandlerPoolTasks.instance.getExecutor().submit(new OnNewlyAddThread(cc));
+            MessageHandlerPoolTasks.instance.getExecutor().submit(new OnNewlyAddThread(pbapnsEvent));
             //心跳
-            ack(ctx, cc, SYNC);
-        }else if(cc.getOp() == PBAPNSEvent.Ops.PushAck_VALUE){
+            ack(ctx, conn, pbapnsEvent, SYNC);
+        }else if(pbapnsEvent.getOp() == PBAPNSEvent.Ops.PushAck_VALUE){
             //推送反馈
-            if (cc.getRead() > 0){
+            if (pbapnsEvent.getRead() > 0){
 
                 MessageHandlerPoolTasks.instance.getExecutor().submit(new Runnable() {
                     @Override
                     public void run() {
-                        ClientServiceImpl.instance.updateBadge(cc.getUserId(), cc.getRead() * -1);
+                        ClientServiceImpl.instance.updateBadge(pbapnsEvent.getUserId(), pbapnsEvent.getRead() * -1);
                     }
                 });
 
             }
 
-            ack(ctx, cc, SYNC);
+            Connection conn = ConnectionKeeper.get(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId());
+            ack(ctx, conn, pbapnsEvent, SYNC);
 
-        }else if(cc.getOp() == PBAPNSEvent.Ops.Offline_VALUE){
+        }else if(pbapnsEvent.getOp() == PBAPNSEvent.Ops.Offline_VALUE){
             //离线
-            final Connection connection = ConnectionKeeper.get(cc.getAppKey(), cc.getUserId());
+            final Connection connection = ConnectionKeeper.get(pbapnsEvent.getAppKey(), pbapnsEvent.getUserId());
             if (connection != null) {
                 ConnectionKeeper.remove(connection.getAppKey(), connection.getUserId());
 
                 connection.close();
-                logger.info("Client disconnect: {}", cc);
+                logger.info("Client disconnect: {}", pbapnsEvent);
 
                 MessageHandlerPoolTasks.instance.getExecutor().submit(new Runnable() {
 
                     @Override
                     public void run() {
-                        Client c0 = ClientServiceImpl.instance.findByUserId(cc.getUserId());
+                        Client c0 = ClientServiceImpl.instance.findByUserId(pbapnsEvent.getUserId());
                         if (c0 != null) {
                             ClientServiceImpl.instance.updateOfflineTs(c0.getId(), connection.getLastOpTime());
                         }
@@ -173,7 +176,7 @@ public class MobileMessageHandler extends ChannelInboundHandlerAdapter {
         }
     }
 
-    private void ack(final ChannelHandlerContext ctx, PBAPNSEvent cc, final String result){
+    private void ack(final ChannelHandlerContext ctx, final Connection cc, PBAPNSEvent event, final String result){
         PBAPNSMessage.Builder builder = PBAPNSMessage.newBuilder();
         builder.setAps(PBAPNSBody.newBuilder().setAlert("ack").setBadge(0));
 
@@ -188,16 +191,16 @@ public class MobileMessageHandler extends ChannelInboundHandlerAdapter {
         final ByteBuf data = ctx.alloc().buffer(bytes.length); // (2)
         data.writeBytes(bytes);
 
-        final ChannelFuture cf = ctx.channel().writeAndFlush(data);
+        final ChannelFuture cf = cc.getChannel().writeAndFlush(data);
         cf.addListener(new GenericFutureListener<Future<? super Void>>() {
             @Override
             public void operationComplete(Future<? super Void> future) throws Exception {
                 if(cf.cause() != null){
                     logger.error("Send Error.", cf.cause());
-                    ctx.close();
+                    cc.close();
                 }else{
                     if (result.equalsIgnoreCase(MULTI_CLIENTS)){
-                        ctx.close();
+                        cc.close();
                     }
                 }
             }

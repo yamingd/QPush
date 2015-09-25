6418bf36bd0e3b8d227af4e06f73b442cd6c4276
Yaming <yamingd@qq.com>
2015-09-25 18:27:42 +0800

    code refactor

--- Deploy/deploy-qpush.sh
+++ Deploy/deploy-qpush.sh
@@ -3,10 +3,7 @@
 cd /home/jetty/source
 
 target="QPush"
-VERSION="1.1"
-SOURCE_FOLDER="/home/jetty/source/QPush"
-RUN_FOLDER="/home/jetty/qpush"
-
+VERSION="1.3"
 
 if [ ! -d "$target" ]; then
   git clone https://github.com/yamingd/QPush.git
@@ -16,40 +13,30 @@ else
   git pull origin master
 fi
 
-# clean first
 gradle clean
-# compile source
 gradle build
-# install to .m2
 gradle install
-# package
 gradle createJobJar
 
-
-\cp -rf $SOURCE_FOLDER/Gateway/build/qpush-gateway-$VERSION-all.jar $RUN_FOLDER/gateway
-cd $RUN_FOLDER/gateway
+\cp -rf /home/jetty/source/QPush/Gateway/build/Gateway-$VERSION-all.jar /home/jetty/qpush/gateway
+cd /home/jetty/qpush/gateway
 rm -fr lib
 rm -fr classes
-unzip -o qpush-gateway-$VERSION-all.jar
+unzip -o Gateway-$VERSION-all.jar
 \cp -rf ../conf/jdbc.properties   ../conf/jedis.properties ../conf/logback.xml ../conf/spring-redis.xml ../conf/spring-db.xml ../conf/gateway.properties classes
 
 \cp /home/jetty/wrapper-linux-x86-64-3.5.26/lib/libwrapper.so lib/
 \cp /home/jetty/wrapper-linux-x86-64-3.5.26/lib/wrapper.jar lib/
+rm Gateway-$VERSION-all.jar
 
-echo $(date) > $RUN_FOLDER/gateway/deploy.status
-
-rm qpush-gateway-$VERSION-all.jar
-
-\cp -rf $SOURCE_FOLDER/Publisher/build/qpush-publisher-$VERSION-all.jar $RUN_FOLDER/publisher
-cd $RUN_FOLDER/publisher
+\cp -rf /home/jetty/source/QPush/Publisher/build/Publisher-$VERSION-all.jar /home/jetty/qpush/publisher
+cd /home/jetty/qpush/publisher
 rm -fr lib
 rm -fr classes
-unzip -o qpush-publisher-$VERSION-all.jar
+unzip -o Publisher-$VERSION-all.jar
 \cp -rf ../conf/jdbc.properties   ../conf/jedis.properties ../conf/logback.xml ../conf/spring-redis.xml ../conf/spring-db.xml ../conf/publisher.properties classes
 
 \cp /home/jetty/wrapper-linux-x86-64-3.5.26/lib/libwrapper.so lib/
 \cp /home/jetty/wrapper-linux-x86-64-3.5.26/lib/wrapper.jar lib/
 
-rm qpush-publisher-$VERSION-all.jar
-
-echo $(date) > $RUN_FOLDER/publisher/deploy.status
+rm Publisher-$VERSION-all.jar
